b1defce85d81cb6d8d8f0b9667d1730a8835446a
sqlwwx <wwx_2012@hotmail.com>
2017-01-13 09:54:50 +0800

    support zazu

---
+++ zazu-entry.js
@@ -0,0 +1,16 @@
+var freeShadowsocks = require('.')
+
+const template = (server) => ({
+  icon: 'fa-globe',
+  title: `${server.password}`,
+  subtitle: `${server.server} ${server.method} ${server.server_port}`,
+  value: server.password
+})
+
+module.exports = (pluginContext) => {
+  return freeShadowsocks().then((servers) => {
+    return Promise.resolve(
+      servers.map(template)
+    )
+  })
+}
